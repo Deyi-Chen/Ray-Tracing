4fa138b787b7baa416d1d4ddaa8348a568afb8cd
Deyi Chen <deyichen36@gmail.com>
2026-04-07 13:03:32 -0700

    Refactor: introduce vec3 and extract color output logic

---
+++ color.h
@@ -0,0 +1,20 @@
+#ifndef COLOR_H
+#define COLOR_H
+#include <iostream>
+#include "vec3.h"
+using color = vec3;
+
+void write_color(std::ostream &out, const color &pixel_color)
+{
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    int ir = int(255.999 * r);
+    int ig = int(255.999 *g);
+    int ib = int(255.999 * b);
+
+    out << ir << ' ' << ig << ' ' << ib << '\n';
+}
+
+#endif
---
+++ vec3.h
@@ -0,0 +1,109 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+
+class vec3{
+    public:
+        double e[3];        vec3(){
+            for(int i=0;i<3;i++){
+                e[i]=0.0;
+            }
+        }
+        vec3(double x, double y, double z){
+            e[0]=x;
+            e[1]=y;
+            e[2]=z;
+        }
+        vec3& operator+=(const vec3&v){
+            for (int i=0;i<3;i++){
+                e[i]+=v.e[i];
+            }
+            return *this;
+        }
+        vec3& operator*=(double t){
+            for (int i=0;i<3;i++){
+                e[i]*=t;
+            }
+            return *this;
+        }
+        vec3& operator/=(double t){
+            for (int i=0;i<3;i++){
+                e[i]/=t;
+            }
+            return *this;
+        }
+        double length_squared()const{
+            double result=0.0;
+            for (int i=0;i<3;i++){
+                result+=e[i]*e[i];
+            }
+            return result;
+        }
+        double length() const{
+            return sqrt(length_squared());
+        }
+        double x() const{
+            return e[0];
+        }
+        double y() const{
+            return e[1];
+        }
+        double z()const{
+            return e[2];
+        }
+
+};
+
+vec3 operator+(const vec3&u,const vec3&v){
+    vec3 result=vec3();
+    for(int i=0;i<3;i++){
+        result.e[i]=u.e[i]+v.e[i]; //addition for doubles
+    }
+    return result; 
+}
+
+vec3 unit_vector(const vec3&v){
+    vec3 result=v;
+    double v_length=v.length();
+    for(int i=0;i<3;i++){
+        result.e[i]/=v_length;
+    }
+    return result;
+}
+
+double dot(const vec3&u,const vec3&v){
+    double result=0.0;
+    for(int i=0;i<3;i++){
+        result+=u.e[i]*v.e[i];
+    }
+    return result;
+}
+
+vec3 operator-(const vec3&u,const vec3&v){
+    vec3 result=vec3();
+    for(int i=0;i<3;i++){ 
+        result.e[i]=u.e[i]-v.e[i];
+    }
+    return result;
+}
+
+vec3 operator*(const vec3&u,double t){
+    vec3 result=vec3();
+    for(int i=0;i<3;i++){ 
+        result.e[i]=u.e[i]*t;
+    }
+    return result;
+}
+
+vec3 operator*(double t,const vec3&u){ 
+    vec3 result=vec3();
+    for(int i=0;i<3;i++){ 
+        result.e[i]=u.e[i]*t;
+    }
+    return result;
+}
+
+
+
+#endif
